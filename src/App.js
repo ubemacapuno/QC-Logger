d722dae4555caf7195d9321af2495d9779022165
corey <damoclescj@gmail.com>
2022-10-25 21:58:38 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+// pages
+import Home from "./pages/Home"
+import Create from "./pages/Create"
+import Update from "./pages/Update"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <h1>QC Buddy</h1>
+        <Link to="/">Home</Link>
+        <Link to="/create">Enter QC Test</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
