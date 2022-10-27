97b886f037ec33d44d7cc48f49cc41c5c71a21bf
corey <damoclescj@gmail.com>
2022-10-27 00:06:25 -0700

    CRUD functions

--- src/components/ReportCard.js
+++ src/components/ReportCard.js
@@ -1,17 +1,25 @@
 import supabase from '../config/supabaseClient'
+import { Link } from 'react-router-dom'
 
 const ReportCard = ({ report }) => {
+    //Logic for Pass/Fail
     let qcResult = "Fail"
-    if(report.pH > 5 && report.pH < 7.5 && report.etOH <= 5 && report.aCN <=0.4){
+    if(report.ph > 5 && report.ph < 7.5 && report.etoh <= 5 && report.acn <=0.4){
         qcResult = "Pass"
     }
-
 
     return (
         <div className="smoothie-card">
             <h3>{report.batch}</h3>
             <p>EOS Date {report.date}</p>
             <div className={qcResult}>{qcResult}</div>
+            
+            {/* Edit and Delete buttons: */}
+            <div className="buttons">
+                <Link to={'/' + report.id}>
+                    <i className="material-icons">edit</i>
+                </Link>
+            </div>
         </div>
     )
 }
