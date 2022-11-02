af08ee1a36f217e9f468e45a25c5a87c3abd0766
corey <damoclescj@gmail.com>
2022-11-01 22:19:14 -0700

    Added Magic Link Auth

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
+import { useState, useEffect } from 'react'
+import supabase from "./config/supabaseClient"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 // pages
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import Update from "./pages/Update"
-import Login from "./pages/Login"
 
+import Auth from './Auth'
+import Account from './Account'
 
-function App() {
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <BrowserRouter>
-      <nav>
-        <div className="flex-header">
-          <i className="material-icons logo">biotech</i>
-          <h1>QC Logger</h1>
+    <div className="container">
+      {!session ? (
+        <Auth />
+      ) : (
+        <div>
+          <BrowserRouter>
+            <nav>
+              <div className="flex-header">
+                <i className="material-icons logo">biotech</i>
+                <h1>QC Logger</h1>
+              </div>
+              <Link to="/">Home</Link>
+              <Link to="/create">Enter QC Results</Link>
+              <Account key={session.user.id} session={session} />
+            </nav>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/:id" element={<Update />} />
+            </Routes>
+          </BrowserRouter>
         </div>
-        <Link to="/">Home</Link>
-        <Link to="/create">Enter QC Results</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/:id" element={<Update />} />
-      </Routes>
-    </BrowserRouter>
-  );
+        
+      )}
+    </div>
+  )
 }
-
-export default App;
--- src/pages/api/auth.js
+++
@@ -1,8 +0,0 @@
-//Setting cookies, creating, removing
-import supabase from "../../config/supabaseClient";
-
-//Helper function provided by supabase for setting and clearing the cookie.
-//We will call this from App.js
-export default function handler(req, res) {
-    supabase.auth.resetPasswordForEmail.setAuthCookie(req, res)
-}
